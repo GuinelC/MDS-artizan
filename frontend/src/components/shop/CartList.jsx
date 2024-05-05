7a9ed2fdbcf0161440b2ddc1b6b2cd5767912fde
Charly Guinel <charly.guinel@gmail.com>
2024-05-05 17:48:57 +0200

    wip shop

--- frontend/src/components/shop/CartList.jsx
+++ frontend/src/components/shop/CartList.jsx
@@ -1,53 +1,78 @@
-import React from 'react';
-import { BiTrash } from 'react-icons/bi';
-import { useCart } from '../../context/cartContext';
+import React from "react";
+import { BiTrash, BiPlus, BiMinus } from "react-icons/bi";
+import { useCart } from "../../context/cartContext";
+import "./shop.css";
 
 function CartList() {
-  const { state: { items, total }, removeFromCart } = useCart();
+  const {
+    state: { items, total },
+    removeFromCart,
+    addToCart,
+    adjustQuantity, // Assurez-vous que adjustQuantity est importé depuis useCart
+  } = useCart();
 
   if (items.length < 1) {
-    return (
-      <h2>Votre panier est vide</h2>
-    );
+    return <h2>Votre panier est vide</h2>;
   }
+
+  const handleDecrement = (itemId) => {
+    // Décrémente simplement la quantité de l'article dans le panier
+    adjustQuantity(itemId);
+  };
 
   return (
     <>
       <h1>Cart</h1>
-      <table border={1}>
-        <thead>
-          <tr>
-            <th>Nom du produit</th>
-            <th>Description du produit</th>
-            <th>Prix unitaire</th>
-            <th>Quantité</th>
-            <th>Prix total</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.map(cartItem => (
-            <tr key={cartItem.item.id}>
-              <td>{cartItem.item.attributes.name}</td>
-              <td>{cartItem.item.attributes.description}</td>
-              <td>{cartItem.item.attributes.price || '10'}€</td>
-              <td>{cartItem.qty}</td>
-              <td>{(cartItem.qty * (cartItem.item.attributes.price || 10)).toFixed(2)} €</td>
-              <td>
-                <button onClick={() => removeFromCart(cartItem.item.id)}>
-                  <BiTrash size={20} />
-                </button>
-              </td>
+      <div className="tabContainer mx-auto">
+        <table className="shopTab" border={1}>
+          <thead>
+            <tr>
+              <th>Nom du produit</th>
+              <th>Description du produit</th>
+              <th>Prix unitaire</th>
+              <th>Quantité</th>
+              <th>Prix total</th>
+              <th>Actions</th>
             </tr>
-          ))}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td>Total</td>
-            <td>{total}</td>
-          </tr>
-        </tfoot>
-      </table>
+          </thead>
+          <tbody>
+            {items.map((cartItem) => (
+              <tr key={cartItem.item.id}>
+                <td>{cartItem.item.attributes.name}</td>
+                <td>{cartItem.item.attributes.description}</td>
+                <td>{cartItem.item.attributes.price || "10"}€</td>
+                <td>
+                  <button onClick={() => handleDecrement(cartItem.item.id)}>
+                    <BiMinus size={20} />
+                  </button>
+                  {cartItem.qty}
+                  <button onClick={() => addToCart(cartItem.item)}>
+                    <BiPlus size={20} />
+                  </button>
+                </td>
+                <td>
+                  {(
+                    cartItem.qty * (cartItem.item.attributes.price || 10)
+                  ).toFixed(2)}{" "}
+                  €
+                </td>
+                <td>
+                  <button onClick={() => removeFromCart(cartItem.item.id)}>
+                    <BiTrash size={20} />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <div className="TotalCard">
+          <h3>TOTAL</h3>
+          <hr />
+          <p>{total}€</p>
+          <button className="shop-submit">PAIEMENT</button>
+        </div>
+      </div>
     </>
   );
 }
--- frontend/src/context/cartContext.jsx
+++ frontend/src/context/cartContext.jsx
@@ -1,81 +1,110 @@
+// Import des modules nécessaires
 import React, { createContext, useContext, useReducer, useEffect } from 'react';
 
+// Création du contexte du panier
 const CartContext = createContext();
 
+// Définition des types d'actions
 const actionTypes = {
   ADD_TO_CART: 'ADD_TO_CART',
   REMOVE_FROM_CART: 'REMOVE_FROM_CART',
-  SET_TOTAL: 'SET_TOTAL', // Ajouter une action pour mettre à jour le total du panier
+  SET_TOTAL: 'SET_TOTAL',
+  ADJUST_QUANTITY: 'ADJUST_QUANTITY', // Ajout du type pour ajuster la quantité
   RESET: 'RESET'
 };
 
+// État initial du panier
 const initialState = {
   items: JSON.parse(localStorage.getItem('cart')) || [],
   total: 0
 };
 
+// Reducer du panier
 const cartReducer = (state, action) => {
   switch (action.type) {
+    // Action pour ajouter un article au panier
     case actionTypes.ADD_TO_CART:
       return {
         ...state,
-        // On vérifie si le panier contient déjà l'item
         items: state.items.some(cartItem => cartItem.item.id === action.data.item.id)
-          // Il est déjà dans le panier : On boucle sur les items pour trouver celui déjà présent
           ? state.items.map(cartItem => {
             if (cartItem.item.id === action.data.item.id) {
-              // On augmente sa quantité de 1
               return { ...cartItem, qty: cartItem.qty + 1 }
             }
             return cartItem
           })
-          // Il n'est pas dans le panier : On ajoute l'item dans le tableau
-          : state.items.concat([{ item: action.data.item, qty: 1 }]) // => item = plat
+          : state.items.concat([{ item: action.data.item, qty: 1 }])
       }
+    // Action pour supprimer un article du panier
     case actionTypes.REMOVE_FROM_CART:
       return {
         ...state,
         items: state.items.filter(cartItem => action.data.id !== cartItem.item.id)
       };
+    // Action pour mettre à jour le total du panier
     case actionTypes.SET_TOTAL:
       return {
         ...state,
         total: action.total
+      };
+    // Action pour ajuster la quantité d'un article dans le panier
+    case actionTypes.ADJUST_QUANTITY:
+      return {
+        ...state,
+        items: state.items.map(cartItem => {
+          if (cartItem.item.id === action.data.id) {
+            const newQty = cartItem.qty - 1 >= 0 ? cartItem.qty - 1 : cartItem.qty;
+            return { ...cartItem, qty: newQty };
+          }
+          return cartItem;
+        }).filter(item => item.qty > 0) // Supprimez les articles avec une quantité de zéro du panier
       };
     default:
       throw new Error(`Unhandled action type: ${action.type}`);
   }
 };
 
-
-const cartFactory = dispatch => ({
-  addToCart: (item, qty = 1) => { // Ajout d'un paramètre 'qty' avec une valeur par défaut de 1
+// Fonctions pour créer des actions de panier
+const cartFactory = (dispatch) => ({
+  // Action pour ajouter un article au panier
+  addToCart: (item, qty = 1) => {
     dispatch({
       type: actionTypes.ADD_TO_CART,
-      data: { item, qty } // Passer la quantité avec l'article
+      data: { item, qty },
     });
   },
-  removeFromCart: id => {
+  // Action pour supprimer un article du panier
+  removeFromCart: (id) => {
     dispatch({
       type: actionTypes.REMOVE_FROM_CART,
-      data: { id }
+      data: { id },
     });
-  }
+  },
+  // Action pour ajuster la quantité d'un article dans le panier
+  adjustQuantity: (id, qty) => {
+    dispatch({
+      type: actionTypes.ADJUST_QUANTITY,
+      data: { id, qty },
+    });
+  },
 });
 
-
+// Provider du panier
 const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(cartReducer, initialState);
 
+  // Effet pour mettre à jour le panier dans le localStorage
   useEffect(() => {
     localStorage.setItem('cart', JSON.stringify(state.items));
   }, [state.items]);
 
+  // Effet pour calculer et mettre à jour le total du panier
   useEffect(() => {
     const total = state.items.reduce((acc, curr) => acc + curr.item.attributes.price * curr.qty, 0);
-    dispatch({ type: actionTypes.SET_TOTAL, total }); // Dispatch l'action pour mettre à jour le total du panier
+    dispatch({ type: actionTypes.SET_TOTAL, total });
   }, [state.items]);
 
+  // Rendu du Provider avec le contexte et les actions du panier
   return (
     <CartContext.Provider value={{ state, ...cartFactory(dispatch) }}>
       {children}
@@ -83,10 +112,12 @@ const CartProvider = ({ children }) => {
   );
 };
 
+// Hook pour utiliser le panier dans les composants
 const useCart = () => {
   const context = useContext(CartContext);
   if (!context) throw new Error('useCart must be used inside a CartProvider');
   return context;
 };
 
+// Export des éléments nécessaires
 export { CartProvider, useCart };
